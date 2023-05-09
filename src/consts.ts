04c34a1637134600ad2bf15b2dec5b55343891c5
dmdboi <padfootdiamond@gmail.com>
2023-05-09 21:42:07 +0100

    Init

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Adonis Addons | Extra packages for AdonisJS 5";
+export const SITE_DESCRIPTION = "A collection of addons for AdonisJS 5 that add extra functionality";
